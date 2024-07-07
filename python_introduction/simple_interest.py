40fc707c58011f8c14abdbc107f2b3dbd6146eda
Mr Ingabire <alaingabire5@gmail.com>
2024-07-07 10:36:39 +0200

    adding simple

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#defining variables
+
+principal = 1000
+rate = 0.05
+time = 3
+
+# finding interest
+
+interest = principal * rate * time
+
+#the outcome
+
+print ( f" The simple interest is :{interest}" )
