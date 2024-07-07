a31791637f3fc02f8fb16f334eb68c956c209da8
Mr Ingabire <alaingabire5@gmail.com>
2024-07-07 10:44:29 +0200

    calculating the area

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+#defining the variables
+
+length = 10
+width = 5
+
+# applying the area formula
+
+area = length * width 
+
+#printing the results
+
+print ( f "The area of the rectangle is: {area}"
