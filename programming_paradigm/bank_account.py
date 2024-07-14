529091a600adb8a765a794435dff0c8076d8b88a
Mr Ingabire <alaingabire5@gmail.com>
2024-07-14 02:20:59 +0200

    bank acc

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 # bank_account.py
 
 class BankAccount:
-    def __init__(self, initial_balance=0):
+    def __float__(self, initial_balance=0):
         self.account_balance = initial_balance
     
     def deposit(self, amount):
